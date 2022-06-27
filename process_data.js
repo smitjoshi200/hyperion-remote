e1efd8347d1713132217046c59fcbe4357a64d25
Indiedude78 <saj39@njit.edu>
2022-06-27 17:40:33 -0400

    Updated remote brightness and game mode panel

--- process_data.js
+++ process_data.js
@@ -1,33 +1,46 @@
-$(document).ready(function() {
+$(document).ready(function () {
     //DOM cache
     var $led_on_button = $("#led_on");
     var $led_off_button = $("#led_off");
-    var $brightness_panel = $("#brightness-control");
+    var $brightness_level = $("#brightness-level");
     var $gamemode_switch = $("#game-mode-toggle");
+    var $brightness_slider = $("#brightness-slider");
     var $red = $("#red");
     var $blue = $("#blue");
     var $green = $("#green");
     var $lime = $("#lime");
     var $yellow = $("#yellow");
-    var $purple = $("purple");
     var $orange = $("#orange");
+    var $color_input = $("#color-input");
+    var $color_val = $("#color-val");
     //Misc variables
     var $led_status = false;
     var $my_window;
-    var timeout = 1000;
-    const $brightness_array = [
-        25,
-        50,
-        75,
-        100
-    ];
-    console.log($brightness_array[0].toString());
+    var timeout = 100;
+
+    $color_input.on("change", function () {
+        $current_color = $color_input.val();
+        $color_val.text($current_color);
+        //comvert hex to rgb
+        var rgb = hexToRgb($current_color);
+        console.log(rgb["r"]);
+    });
+    function hexToRgb($hex) {
+        var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec($hex);
+        return result ? {
+            r: parseInt(result[1], 16),
+            g: parseInt(result[2], 16),
+            b: parseInt(result[3], 16)
+        } : null;
+    }
+
+
 
     function windowOperation($window, $command) {
         $window = window.open($command);
         $window.blur()
         window.focus();
-        setTimeout(function() {
+        setTimeout(function () {
             $window.close()
         }, timeout);
     }
@@ -35,14 +48,14 @@ $(document).ready(function() {
     function controlLEDS() {
         var $toggle_lights = '';
         //Detect button click
-        $led_on_button.click(function() {
+        $led_on_button.click(function () {
             console.log("LEDs turned on");
             $led_status = true;
             $toggle_lights = 'http://192.168.1.117:8090/json-rpc?request=%7B%22command%22:%22componentstate%22,%22componentstate%22:%7B%22component%22:%22ALL%22,%22state%22:' + $led_status.toString() + '%7D%7D';
             windowOperation($my_window, $toggle_lights);
         }); // On button clicked
 
-        $led_off_button.click(function() {
+        $led_off_button.click(function () {
             console.log("LEDs turned off");
             $led_status = false;
             $toggle_lights = 'http://192.168.1.117:8090/json-rpc?request=%7B%22command%22:%22componentstate%22,%22componentstate%22:%7B%22component%22:%22ALL%22,%22state%22:' + $led_status.toString() + '%7D%7D';
@@ -50,46 +63,21 @@ $(document).ready(function() {
         }); //Off button clicked
     }
 
-    for (let i = 0; i < $brightness_array.length; i++) {
-        $brightness_panel.append("<button class='button' id='brightness_" +
-            $brightness_array[i] + "'>" +
-            $brightness_array[i] +
-            "</button>")
-    }
-
-    //DOM Cache (Brightness Buttons)
-    var $low = $("#brightness_25");
-    var $medium = $("#brightness_50");
-    var $high = $("#brightness_75");
-    var $highest = $("#brightness_100");
-
     function controlBrightness() {
         var $set_brightness = '';
-        $low.click(function() {
-            console.log("Brightness set to 25%");
-            $set_brightness = "http://192.168.1.117:8090/json-rpc?request=%7B%22command%22:%22adjustment%22,%22adjustment%22:%7B%22classic_config%22:false,%22brightness%22:" + $brightness_array[0].toString() + "%7D%7D";
-            windowOperation($my_window, $set_brightness);
-        });
-        $medium.click(function() {
-            console.log("Brightness set to 50%");
-            $set_brightness = "http://192.168.1.117:8090/json-rpc?request=%7B%22command%22:%22adjustment%22,%22adjustment%22:%7B%22classic_config%22:false,%22brightness%22:" + $brightness_array[1].toString() + "%7D%7D";
-            windowOperation($my_window, $set_brightness);
-        });
-        $high.click(function() {
-            console.log("Brightness set to 75%");
-            $set_brightness = "http://192.168.1.117:8090/json-rpc?request=%7B%22command%22:%22adjustment%22,%22adjustment%22:%7B%22classic_config%22:false,%22brightness%22:" + $brightness_array[2].toString() + "%7D%7D";
-            windowOperation($my_window, $set_brightness);
-        });
-        $highest.click(function() {
-            console.log("Brightness set to 100%");
-            $set_brightness = "http://192.168.1.117:8090/json-rpc?request=%7B%22command%22:%22adjustment%22,%22adjustment%22:%7B%22classic_config%22:false,%22brightness%22:" + $brightness_array[3].toString() + "%7D%7D";
+        //Detect brightness slider change
+        $brightness_slider.on("change", function () {
+            var $brightness_value = $brightness_slider.val();
+            $brightness_level.text($brightness_value + "%");
+            console.log("Brightness set to " + $brightness_value + "%");
+            $set_brightness = "http://192.168.1.117:8090/json-rpc?request=%7B%22command%22:%22adjustment%22,%22adjustment%22:%7B%22classic_config%22:false,%22brightness%22:" + $brightness_value.toString() + "%7D%7D";
             windowOperation($my_window, $set_brightness);
         });
     }
 
     function toggleGamemode() {
         var $set_gamemode = '';
-        $gamemode_switch.click(function() {
+        $gamemode_switch.click(function () {
             if ($(this).prop("checked") == true) {
                 console.log("Gamemode On");
                 $set_gamemode = "http://192.168.1.117:8090/json-rpc?request=%7B%20%20%22command%22:%22color%22,%20%20%22color%22:%5B0,37,255%5D,%20%20%22duration%22:0,%20%20%22priority%22:100,%20%20%22origin%22:%22JSON%20API%22%7D";
@@ -100,28 +88,13 @@ $(document).ready(function() {
                 windowOperation($my_window, $set_gamemode);
             }
         });
-        $red.click(function() {
-            $set_gamemode = "http://192.168.1.117:8090/json-rpc?request=%7B%20%20%22command%22:%22color%22,%20%20%22color%22:%5B255,0,0%5D,%20%20%22duration%22:0,%20%20%22priority%22:100,%20%20%22origin%22:%22JSON%20API%22%7D";
-            windowOperation($my_window, $set_gamemode);
-        });
-        $blue.click(function() {
-            $set_gamemode = "http://192.168.1.117:8090/json-rpc?request=%7B%20%20%22command%22:%22color%22,%20%20%22color%22:%5B0,0,255%5D,%20%20%22duration%22:0,%20%20%22priority%22:100,%20%20%22origin%22:%22JSON%20API%22%7D";
-            windowOperation($my_window, $set_gamemode);
-        });
-        $green.click(function() {
-            $set_gamemode = "http://192.168.1.117:8090/json-rpc?request=%7B%20%20%22command%22:%22color%22,%20%20%22color%22:%5B0,255,5%5D,%20%20%22duration%22:0,%20%20%22priority%22:100,%20%20%22origin%22:%22JSON%20API%22%7D";
-            windowOperation($my_window, $set_gamemode);
-        });
-        $lime.click(function() {
-            $set_gamemode = "http://192.168.1.117:8090/json-rpc?request=%7B%20%20%22command%22:%22color%22,%20%20%22color%22:%5B165,255,0%5D,%20%20%22duration%22:0,%20%20%22priority%22:100,%20%20%22origin%22:%22JSON%20API%22%7D";
-            windowOperation($my_window, $set_gamemode);
-        });
-        $yellow.click(function() {
-            $set_gamemode = "http://192.168.1.117:8090/json-rpc?request=%7B%20%20%22command%22:%22color%22,%20%20%22color%22:%5B250,255,0%5D,%20%20%22duration%22:0,%20%20%22priority%22:100,%20%20%22origin%22:%22JSON%20API%22%7D";
-            windowOperation($my_window, $set_gamemode);
-        });
-        $orange.click(function() {
-            $set_gamemode = "http://192.168.1.117:8090/json-rpc?request=%7B%20%20%22command%22:%22color%22,%20%20%22color%22:%5B245,145,0%5D,%20%20%22duration%22:0,%20%20%22priority%22:100,%20%20%22origin%22:%22JSON%20API%22%7D";
+        $color_input.on("change", function () {
+            $current_color = $color_input.val();
+            $color_val.text($current_color);
+            //convert hex to rgb
+            var rgb = hexToRgb($current_color);
+            console.log(rgb["r"]);
+            $set_gamemode = "http://192.168.1.117:8090/json-rpc?request=%7B%20%20%22command%22:%22color%22,%20%20%22color%22:%5B" + rgb["r"].toString() + "," + rgb["g"].toString() + "," + rgb["b"].toString() + "%5D,%20%20%22duration%22:0,%20%20%22priority%22:100,%20%20%22origin%22:%22JSON%20API%22%7D";
             windowOperation($my_window, $set_gamemode);
         });
     }
