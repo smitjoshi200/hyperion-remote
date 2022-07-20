e87dc276265a81fcfbac0cfb417715961c9f1a34
Indiedude78 <saj39@njit.edu>
2022-07-20 09:51:40 -0400

    Updating gitignore and adding config file for api server url

--- process_requests.php
+++ process_requests.php
@@ -1,10 +1,10 @@
 <?php
-
+require_once(__DIR__ . "/api_server.php");
 $data = json_decode(file_get_contents('php://input'), true);
 //echo var_dump($data);
 $command = json_encode($data);
 $ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, "http://192.168.1.117:8090/json-rpc");
+curl_setopt($ch, CURLOPT_URL, $api_url);
 curl_setopt($ch, CURLOPT_POST, 1);
 curl_setopt($ch, CURLOPT_POSTFIELDS, $command);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
